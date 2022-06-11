6af3fea8d3b7064d1cbfeb85dd203e9378ea3c8d
Jacob Hawkins <jacobhawkins7@gmail.com>
2022-06-11 14:14:26 -0700

    Base Game building. Having fun

--- Ch.16/code/chapter/16_game.js
+++ Ch.16/code/chapter/16_game.js
@@ -76,6 +76,41 @@ var Player = class Player {
 
 Player.prototype.size = new Vec(0.8, 1.5);
 
+var Monster = class Monster {
+  constructor(pos) {
+    this.pos = pos;
+    this.speed = 4;
+  }
+
+  get type() {
+    return 'monster';
+  }
+
+  static create(pos) {
+    return new Monster(pos.plus(new Vec(0, -1)));
+  }
+
+  update(time, state) {
+    let player = state.player;
+    let speed = (player.pos.x < this.pos.x ? -1 : 1) * time * this.speed;
+    let newPos = new Vec(this.pos.x + speed, this.pos.y);
+    if (state.level.touches(newPos, this.size, 'wall')) return this;
+    else return new Monster(newPos);
+  }
+
+  collide(state) {
+    let player = state.player;
+    if (player.pos.y + player.size.y < this.pos.y + 0.5) {
+      let filtered = state.actors.filter((a) => a != this);
+      return new State(state.level, filtered, state.status);
+    } else {
+      return new State(state.level, state.actors, 'lost');
+    }
+  }
+};
+
+Monster.prototype.size = new Vec(1.2, 2);
+
 var Lava = class Lava {
   constructor(pos, speed, reset) {
     this.pos = pos;
@@ -124,6 +159,7 @@ var levelChars = {
   '#': 'wall',
   '+': 'lava',
   '@': Player,
+  M: Monster,
   o: Coin,
   '=': Lava,
   '|': Lava,
@@ -338,6 +374,10 @@ function trackKeys(keys) {
   }
   window.addEventListener('keydown', track);
   window.addEventListener('keyup', track);
+  down.unregister = () => {
+    window.removeEventListener('keydown', track);
+    window.removeEventListener('keyup', track);
+  };
   return down;
 }
 
@@ -360,8 +400,30 @@ function runLevel(level, Display) {
   let display = new Display(document.body, level);
   let state = State.start(level);
   let ending = 1;
+  let running = 'yes';
+
   return new Promise((resolve) => {
-    runAnimation((time) => {
+    function escHandler(event) {
+      if (event.key != 'Escape') return;
+      event.preventDefault();
+      if (running == 'no') {
+        running = 'yes';
+        runAnimation(frame);
+      } else if (running == 'yes') {
+        running = 'pausing';
+      } else {
+        running = 'yes';
+      }
+    }
+    window.addEventListener('keydown', escHandler);
+    let arrowKeys = trackKeys(['ArrowLeft', 'ArrowRight', 'ArrowUp']);
+
+    function frame(time) {
+      if (running == 'pausing') {
+        running = 'no';
+        return false;
+      }
+
       state = state.update(time, arrowKeys);
       display.syncState(state);
       if (state.status == 'playing') {
@@ -371,24 +433,27 @@ function runLevel(level, Display) {
         return true;
       } else {
         display.clear();
+        window.removeEventListener('keydown', escHandler);
+        arrowKeys.unregister();
         resolve(state.status);
         return false;
       }
-    });
+    }
+    runAnimation(frame);
   });
 }
 
 async function runGame(plans, Display) {
   let lives = 3;
   for (let level = 0; level < plans.length && lives > 0; ) {
-    console.log(`Level ${level + 1}, lives: ${lives}`);
+    alert(`Level ${level + 1}, lives: ${lives}`);
     let status = await runLevel(new Level(plans[level]), Display);
     if (status == 'won') level++;
     else lives--;
   }
   if (lives > 0) {
-    console.log("You've won!");
+    alert("You've won!");
   } else {
-    console.log('Game Over');
+    alert('Game Over');
   }
 }
